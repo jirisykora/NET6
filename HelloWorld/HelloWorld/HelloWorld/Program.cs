162f236cde7f5907b2d3ecd426eff8ce77c6f464
jirisykora <sykorj1@gmail.com>
2021-10-04 12:02:42 +0200

    1. program

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AddAndPrint(10, 45);
+        }
+
+        static void AddAndPrint(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine(sum);
         }
     }
 }
