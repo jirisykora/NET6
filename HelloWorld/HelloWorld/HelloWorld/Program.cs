434b0fe336e02ca271084db2468b9f9339bd8145
jirisykora <sykorj1@gmail.com>
2021-10-04 14:12:50 +0200

    swap variables

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,6 +6,17 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            int a = 20;
+            int b = 30;
+
+            Console.WriteLine($"a={a}, b={b}");
+            
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"a={a}, b={b}");
+
             Console.WriteLine("Hello World!");
             AddAndPrint(10, 45);
             Hello("Jiri");
