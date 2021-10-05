ded09da092d8d33529cea5cf3c479cc815bea7ed
jirisykora <sykorj1@gmail.com>
2021-10-05 12:02:26 +0200

    class person

---
+++ HelloWorld/HelloWorld/HelloWorld/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        // KONSTRUKTOR - vykonává se při vytvoření instance, např. defaultní hodnoty v properties
+        // mohu mít několik konstruktorů - včetně parametrů
+        public Person()
+        {
+            FirstName = "-----";
+            LastName = "-----";
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+        // PROPERTIES - definice vlastností (Property) - nastaveno čtení i zápis pomocí get/set
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        // metoda - statická = metoda na třídě(nevztahuje se k instanci)
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        // přepis děděné metody - override
+        public override string ToString()
+        {
+            return GetFullName();
+        }
+    }
+}
--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,21 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person("Jiri", "Sykora");
+            p1.Age = 54;
 
-            Skoleni.Sum();
+            Person p2 = new Person("Jan", "Novák", 25);
 
-
-                    
-            
-
+            Console.WriteLine($"Osoba 1: {p1}");
+            Console.WriteLine($"Osoba 2: {p2}");
         }
-
-
-        
-
-        
-        
-
-       
     }
 }
